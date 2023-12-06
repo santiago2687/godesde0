199b15e6225cb7b37276cc037dced8aa10614883
santiago2687 <santiago2687@gmail.com>
2023-12-06 18:11:47 -0300

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/santiago2687/godesde0/variables"
 )
 
 func main() {
 	variables.MuestroEnteros()
+	variables.RestoVariables()
+
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
