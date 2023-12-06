5746ac8bbb0e2c755d8d0f7343f938c8612361c6
santiago2687 <santiago2687@gmail.com>
2023-12-06 17:38:02 -0300

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/santiago2687/godesde0
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/santiago2687/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
